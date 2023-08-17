4c779eb191cc7579051bac628735dc7d0326413f
Irakli Pilauri <ipilauri@vaba.co>
2023-08-17 14:53:02 +0400

    adding passwords to the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :project
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
   root to: 'projects#index'
   resources :projects
+  get 'signup', to: 'users#new'
   end
